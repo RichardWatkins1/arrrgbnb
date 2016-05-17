575f63ebeb0cf58db798ddd362e81639a20a510f
naridas <gregsparta@gmail.com>
2016-05-17 11:50:55 +0100

    1-1 model added problem with rspec

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,32 @@
 require 'sinatra/base'
+require_relative 'data_mapper_setup'
+require 'sinatra/flash'
 
 class Arrrgbnb < Sinatra::Base
-  get '/' do
-    'Hello Arrrgbnb!'
+  # get '/' do
+  #   redirect
+  # end
+
+  get '/users/new' do
+    @user = User.new
+    erb :'users/new'
+  end
+
+  post '/users' do
+    @user = User.new( password:params[:password],
+                         password_confirmation:params[:password_confirmation],
+                         email:params[:email],
+                         username:params[:username],
+                         phone:params[:phone],
+                         name:params[:name]
+                       )
+    if @user.save
+      'Welcome, alice@example.com'
+      # redirect '/sessions/new'
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :'users/new'
+    end
   end
 
   # start the server if ruby file executed directly
