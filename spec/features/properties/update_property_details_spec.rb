db86bd698c063216327cdb40c7c59c4bd751c088
naridas <gregsparta@gmail.com>
2016-05-19 18:15:53 +0100

    tons of edge cases solved ERMAHERGERD oh we total made rspec tests...

--- spec/features/properties/update_property_details_spec.rb
+++ spec/features/properties/update_property_details_spec.rb
@@ -4,17 +4,9 @@ feature 'Update property details' do
     sign_up
     create_property_1
     visit '/users/dashboard'
-    click_button 'Edit Property Details'
+    click_button 'Edit'
     expect(current_path).to eq('/property/edit')
-    fill_in :title, with: 'Big House'
-    fill_in :location, with: 'London'
-    fill_in :bedrooms, with: 4
-    fill_in :sleeps, with: 8
-    fill_in :photo, with: 'www.myhousephoto.com'
-    fill_in :price, with: 50
     fill_in :property_type, with: 'Farm'
-    fill_in :date_available_to, with: '2016-05-30'
-    fill_in :date_available_from, with: '2016-05-14'
     click_button 'Confirm Changes'
     expect(Property.last.property_type).to eq 'Farm'
   end
