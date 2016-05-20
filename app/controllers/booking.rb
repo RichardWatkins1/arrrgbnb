390b4ecc490844bac5e218fbc95d1d67e46bbd6b
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-20 12:31:48 +0100

    34-EVERYTHING WORKS FINE AND ALWAYS HAS & edge case

--- app/controllers/booking.rb
+++ app/controllers/booking.rb
@@ -1,32 +1,42 @@
 class Arrrgbnb < Sinatra::Base
 
   get '/booking/new' do
-     @property = Property.get(params[:id])
+     @property ||= Property.get(params[:id])
     erb :'bookings/new'
   end
 
   post '/booking/all' do
     date = params[:date]
-    if (date > current_property.date_available_to &&
-       date < current_property.date_available_from) ||
-        (current_property.bookings.dates.include?(date))
-        flash.now[:errors] = ['Sorry, property is unavailable for that date']
-        redirect "booking/new"
-    end
-    booking = Booking.create( pending: true,
-                            approved: false,
-                            confirmed: false,
-                            rejected: false,
-                            cancelled: false,
-                            completed: false,
-                            date: params[:date]
-                            )
-    current_user.bookings << booking
-    current_user.save
     current_property = Property.get(params[:id])
-    current_property.bookings << booking
-    current_property.save
-    redirect "/users/dashboard"
+    current_bookings = Booking.all(property_id: params[:id])
+    match = 0
+    current_bookings.each do |booking|
+      if booking.date.to_s == date
+        match +=1
+      end
+    end
+    if (date > current_property.date_available_to.to_s || date < current_property.date_available_from.to_s || match > 0)
+       flash.now[:errors] = ['Sorry, property is unavailable for that date']
+          @property = current_property
+          erb :'bookings/new'
+    else
+      booking = Booking.create( pending: true,
+                              approved: false,
+                              confirmed: false,
+                              rejected: false,
+                              cancelled: false,
+                              completed: false,
+                              date: params[:date],
+                              property_id: current_property.id
+                              )
+      current_user.bookings << booking
+      current_user.save
+      current_property.bookings << booking
+      current_property.save
+      redirect "/users/dashboard"
+
+    end
+
   end
 
 
